6e94958c90c6cd64498dc68f34016858d67be83a
Nandhakumar <nandhuhandsome2000@gmail.com>
2024-11-27 01:12:36 +0530

    Implemented Service classes to handle business logic and communicate with repository classes.

---
+++ src/main/java/com/trimble/trimblecars/service/CarService.java
@@ -0,0 +1,211 @@
+package com.trimble.trimblecars.service;
+
+import com.trimble.trimblecars.entity.*;
+import com.trimble.trimblecars.exception.ActionNotAllowedException;
+import com.trimble.trimblecars.exception.FieldMissingException;
+import com.trimble.trimblecars.exception.NotFoundException;
+import com.trimble.trimblecars.repo.CarRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * CarService.java
+ *
+ * <p>
+ * Service layer for managing car-related operations, including registering, updating, and querying cars.
+ * Ensures validation and business rules are applied.
+ * </p>
+ * @author Nandhakumar N 
+ * @module com.trimble.trimblecars.service
+ * @created Nov 23, 2024
+ */
+@Service
+public class CarService
+{
+    private static final Logger log = LogManager.getLogger();
+
+    @Autowired
+    private CarRepository carRepository;
+
+    /**
+     * Registers a car with the given owner ID.
+     *
+     * @param car     the car to be registered
+     * @param user    the owner registering the car
+     * @return the registered car
+     */
+    public Car registerCar(Car car, User user)
+    {
+        log.info("Registering car with license plate: {} for owner ID: {}", car.getLicensePlateNumber(), user.getEmail());
+
+        if(user.getRole().equals(Role.END_CUSTOMER))
+        {
+            throw new ActionNotAllowedException("Customer (Email ID: " + user.getEmail() +") cannot register cars");
+        }
+
+        car.setOwner(user);
+        return registerCar(car);
+    }
+
+    /**
+     * Validates and registers a car.
+     *
+     * @param car the car to be registered
+     * @return the registered car
+     */
+    public Car registerCar(Car car)
+    {
+        validateCar(car);
+        validateDuplicateEntry(car);
+        log.info("Car with license plate: {} successfully validated", car.getLicensePlateNumber());
+        return carRepository.save(car);
+    }
+
+    public Car saveCar(Car car)
+    {
+        return carRepository.save(car);
+    }
+
+    /**
+     * Updates the status of an existing car.
+     *
+     * @param carId  the ID of the car to update
+     * @param status the new status of the car
+     * @param user the owner of the car
+     * @return the updated car
+     */
+    public Car updateCar(Long carId, String status, User user)
+    {
+        Status carStatus = Status.valueOf(status);
+
+        log.info("Updating car ID: {} to status: {}", carId, status);
+
+        log.info("Before calling get Cars");
+
+        Car carFromDB = getCarById(carId);
+
+        log.info("Get cars from DB {}", carFromDB);
+
+        if(!Objects.equals(carFromDB.getOwner().getUserId(), user.getUserId()))
+        {
+            throw new ActionNotAllowedException("Attempted to set car ID: " + carId + " where owner differs, which is not allowed");
+        }
+
+        if(carStatus == Status.ON_LEASE)
+        {
+            throw new ActionNotAllowedException("Attempted to set car ID: " + carId +" to ON_LEASE directly, which is not allowed");
+        }
+
+
+        if(carFromDB.getStatus().equals(Status.ON_LEASE))
+        {
+            throw new ActionNotAllowedException("Attempted to set car ID: " + carId + " which is ON_LEASE to ON_SERVICE directly, which is not allowed");
+        }
+
+        if(carFromDB.getStatus().equals(carStatus))
+        {
+            log.info("Car ID: {} is already in the desired status: {}", carId, status);
+            return carFromDB;
+        }
+
+        carFromDB.setStatus(carStatus);
+
+        return saveCar(carFromDB);
+    }
+
+    /**
+     * Fetches all cars owned by a specific owner.
+     *
+     * @param user the owner
+     * @return list of cars owned by the user
+     */
+    public List<Car> getAllCarsByOwner(User user)
+    {
+        log.info("Fetching all cars for emailID: {}", user.getEmail());
+
+        if(user.getRole().equals(Role.END_CUSTOMER))
+        {
+            throw new ActionNotAllowedException("Access denied: Customer (ID: " + user.getEmail() + ") cannot fetch owned cars");
+        }
+
+        return carRepository.findAllByOwner(user);
+    }
+
+    /**
+     * Fetches all cars available for lease.
+     *
+     * @return list of cars in IDLE status
+     */
+    public List<Car> getAvailableCars()
+    {
+        log.info("Fetching all available cars for lease");
+
+        return getAllCars().stream().filter(car -> car.getStatus() == Status.IDLE).toList();
+    }
+
+    /**
+     * Fetches all cars currently on lease.
+     *
+     * @return list of cars in ON_LEASE status
+     */
+    public List<Car> getLeasedCars()
+    {
+        log.info("Fetching all leased cars");
+
+        return getAllCars().stream().filter(car -> car.getStatus() == Status.ON_LEASE).toList();
+    }
+
+    /**
+    * @throws FieldMissingException if required fields are missing
+    * */
+    private void validateCar(Car car)
+    {
+        if(car.getMake() == null)
+            throw new FieldMissingException("Field 'make' is required!");
+
+        if(car.getModel() == null)
+            throw new FieldMissingException("Field 'model' is required!");
+
+        if(car.getYear() == 0)
+            throw new FieldMissingException("Field 'year' is required!");
+
+        if(car.getLicensePlateNumber() == null)
+            throw new FieldMissingException("Field 'carnumber' is required!");
+    }
+
+    /**
+     * @throws ActionNotAllowedException if any duplicate registration occurs
+    * */
+    private void validateDuplicateEntry(Car car)
+    {
+        boolean isCarExists = carRepository.existsCarByLicensePlateNumber(car.getLicensePlateNumber());
+
+        if(isCarExists) {
+            throw new ActionNotAllowedException("Car with number '" + car.getLicensePlateNumber() +"' already registered");
+        }
+    }
+
+    /**
+     * Fetched car with given id
+     *
+     * @param id The id of the car to be fetched
+     * @throws NotFoundException if not found in records
+    * */
+    public Car getCarById(Long id)
+    {
+        return carRepository.findById(id).orElseThrow(() -> new NotFoundException("Car with id " + id + " is not found in records"));
+    }
+
+    /**
+     * Fetched all cars from DB
+    * */
+    public List<Car> getAllCars()
+    {
+        return carRepository.findAll();
+    }
+}
---
+++ src/main/java/com/trimble/trimblecars/service/LeaseService.java
@@ -0,0 +1,203 @@
+package com.trimble.trimblecars.service;
+
+import com.trimble.trimblecars.entity.*;
+import com.trimble.trimblecars.exception.ActionNotAllowedException;
+import com.trimble.trimblecars.exception.NotFoundException;
+import com.trimble.trimblecars.repo.LeaseRepository;
+import com.trimble.trimblecars.utils.CommonUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * LeaseService.java
+ * <p>
+ * Service class for managing car lease operations.
+ * Handles lease start, end, and history functionalities while applying validation rules.
+ * @author Nandhakumar N 
+ * @module com.trimble.trimblecars.service
+ * @created Nov 23, 2024
+ */
+
+@Service
+public class LeaseService
+{
+    private static final Logger logger = LogManager.getLogger();
+
+    @Autowired
+    private LeaseRepository leaseRepository;
+
+    @Autowired
+    private CarService carService;
+
+    @Autowired
+    private ReportService reportService;
+
+
+    /**
+     * Starts a lease for the specified car and user.
+     *
+     * @param carId  the ID of the car to lease
+     * @param user the user starting the lease
+     * @return the created Lease object
+     */
+    public Lease startLease(Long carId, User user)
+    {
+        logger.info("Starting lease for carId: {} by userId: {}", carId, user);
+
+        Car car = carService.getCarById(carId);
+
+        String email = user.getEmail();
+
+        if(user.getRole().equals(Role.CAR_OWNER))
+        {
+            throw new ActionNotAllowedException("User with ID " + email + " is a car owner and cannot start a lease");
+        }
+
+        List<Lease> leases = user.getLeases();
+
+        // Validate user lease limits
+        if(validateActiveLease(leases))
+        {
+            throw new ActionNotAllowedException("User with ID " + email + " has already leased 2 cars");
+        }
+
+        // Validate car availability
+        if(!car.getStatus().equals(Status.IDLE))
+        {
+            throw new ActionNotAllowedException("Car with ID " + carId +" is not available for leasing. Current status : " +car.getStatus());
+        }
+
+        // Create a new lease
+        Lease lease = new Lease();
+        lease.setCar(car);
+        lease.setCustomer(user);
+        lease.setStartDate(LocalDateTime.now());
+
+        // Update car status to ON_LEASE
+        car.setStatus(Status.ON_LEASE);
+        carService.saveCar(car);
+
+        logger.info("Lease started successfully for carId: {} by userId: {}", carId, email);
+        return leaseRepository.save(lease);
+    }
+
+    /**
+     * Ends a lease for the specified lease ID and user.
+     *
+     * @param leaseId the ID of the lease to end
+     * @param user  the user ending the lease
+     * @return the updated Lease object
+     */
+    public Lease endLease(Long leaseId, User user)
+    {
+        String email = user.getEmail();
+
+        logger.info("Ending lease with ID: {} by {}", leaseId, email);
+
+        Lease lease = getLeaseById(leaseId);
+
+        logger.debug("Lease retrieved: {}, User found: {}", lease, user);
+
+        if(!isValidUser(lease, user))
+        {
+            throw new ActionNotAllowedException("User with ID " + email + " is not authorized to end lease ID "+ leaseId);
+        }
+
+        if(lease.getState().equals(State.ENDED))
+        {
+            throw new ActionNotAllowedException("Lease with ID " + leaseId + " already ended");
+        }
+
+        // End the lease and update car status
+        Car car = lease.getCar();
+        car.setStatus(Status.IDLE);
+
+        lease.setEndDate(LocalDateTime.now());
+
+        carService.saveCar(car);
+
+        logger.info("Lease with ID: {} ended successfully by userId: {}", leaseId, email);
+
+        return leaseRepository.save(lease);
+    }
+
+    /**
+     * Validates if the user is authorized to manage the lease.
+     *
+     * @param lease the lease object
+     * @param user  the user attempting the action
+     * @return true if the user is authorized, false otherwise
+     */
+    private boolean isValidUser(Lease lease, User user)
+    {
+        return user.getUserId().equals(lease.getCustomer().getUserId()) || user.getRole().equals(Role.ADMIN);
+    }
+
+    /**
+     * Retrieves the lease history for a specific user.
+     *
+     * @param user the user object
+     * @return a list of Lease objects associated with the user
+     */
+    public List<Lease> getLeaseHistoryForUser(User user)
+    {
+        logger.info("Fetching lease history for email: {}", user.getEmail());
+
+        if(user.getRole().equals(Role.CAR_OWNER))
+        {
+            throw new ActionNotAllowedException("Car owner (Email ID: " + user.getEmail() + ") can't have lease history");
+        }
+
+        return leaseRepository.findAllByCustomer(user);
+    }
+
+    /**
+     * Retrieves the complete lease history.
+     *
+     * @return a list of all Lease objects
+     */
+    public List<Lease> getAllLeaseHistory()
+    {
+        logger.info("Fetching all lease history");
+        return leaseRepository.findAll();
+    }
+
+    /**
+     * Retrieves a specific lease by its ID.
+     *
+     * @param leaseId the ID of the lease
+     * @return the Lease object
+     */
+    public Lease getLeaseById(Long leaseId)
+    {
+        logger.info("Fetching lease with ID: {}", leaseId);
+
+        return leaseRepository.findById(leaseId)
+                .orElseThrow(() -> new NotFoundException("Lease data not found in records"));
+    }
+
+    private boolean validateActiveLease(List<Lease> leases)
+    {
+        if(CommonUtils.nullOrEmpty(leases))
+            return false;
+        return leases
+                .stream()
+                .filter(lease -> lease.getState().equals(State.ACTIVE))
+                .count() >= 2;
+    }
+
+    public byte[] getLeaseHistoryAsPDF()
+    {
+        return reportService.generateLeaseHistoryReport(getAllLeaseHistory());
+    }
+
+    public byte[] getLeaseHistoryForUserAsPDF(User user)
+    {
+        return reportService.generateLeaseHistoryReport(getLeaseHistoryForUser(user));
+    }
+}
