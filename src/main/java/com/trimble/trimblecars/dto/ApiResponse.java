49d3d7920e31e68dac9315188fa1bf79d156707d
Nandhakumar <nandhuhandsome2000@gmail.com>
2024-11-27 15:19:09 +0530

    Added JsonInclude.Include.NON_NULL to ignore null field in json response.

--- src/main/java/com/trimble/trimblecars/dto/ApiResponse.java
+++ src/main/java/com/trimble/trimblecars/dto/ApiResponse.java
@@ -1,5 +1,6 @@
 package com.trimble.trimblecars.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 
 
@@ -7,6 +8,7 @@ import lombok.*;
 @AllArgsConstructor
 @Data
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResponse<T>
 {
     private ResponseType responseType;
