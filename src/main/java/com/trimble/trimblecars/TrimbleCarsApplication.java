fc9b8fb11bd01d3f87888eaffd1985faac161c1f
Nandhakumar <nandhuhandsome2000@gmail.com>
2024-11-27 14:55:20 +0530

    Added Test for Service classes using junit and mockito.

--- src/main/java/com/trimble/trimblecars/TrimbleCarsApplication.java
+++ src/main/java/com/trimble/trimblecars/TrimbleCarsApplication.java
@@ -2,8 +2,10 @@ package com.trimble.trimblecars;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.trimble.trimblecars"}) //For Junit Test
 public class TrimbleCarsApplication {
 
     public static void main(String[] args) {
